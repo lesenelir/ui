cc7bab274be2b34635b8618ae8c1bb029e434429
lesenelir <hi@lesenelir.me>
2025-09-29 16:24:37 +0800

    feat: add link to Storybook documentation in homepage

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div>hello world</div>
+  return (
+    <div className={'p-4 text-center mt-24'}>
+      <div>
+        <Link href='/storybook' className={'underline text-ac'}>
+          查看 Storybook 组件文档
+        </Link>
+      </div>
+    </div>
+  )
 }
