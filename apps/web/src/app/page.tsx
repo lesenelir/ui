ef3f1c83e88ad952043240c76565f25528e73e9f
lesenelir <hi@lesenelir.me>
2025-09-29 15:02:11 +0800

    chore: cleanup

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from '@lesenelir/ui/button'
-
 export default function Home() {
-  return (
-    <Button appName='web' className={'bg-fg'}>
-      Hello from your web app!
-    </Button>
-  )
+  return <div>hello world</div>
 }
