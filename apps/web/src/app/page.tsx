a6893367d6e37aa9683a09a0a5f3816b6c91151f
lesenelir <hi@lesenelir.me>
2025-10-09 14:32:34 +0800

    feat: add t components

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 
+import { T } from '@/components/t'
+
 export default function Home() {
   return (
     <div className={'p-4 text-center mt-24'}>
-      <div>
-        <Link href='/storybook' className={'underline text-ac'}>
-          View Storybook
-        </Link>
-      </div>
+      <Link href='/storybook' className={'underline text-ac'}>
+        View Storybook
+      </Link>
+
+      <T />
     </div>
   )
 }
