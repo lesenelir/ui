fbc4651cd491b132071a63784ba71f90eb1d3feb
lesenelir <hi@lesenelir.me>
2025-10-30 16:59:09 +0800

    feat: simplify Separator component by removing unused props and optimizing styles

--- packages/ui/src/components/separator/separator.tsx
+++ packages/ui/src/components/separator/separator.tsx
@@ -5,18 +5,13 @@ import type React from 'react'
 import { cn } from '@lesenelir/ui/lib/utils'
 import * as SeparatorPrimitive from '@radix-ui/react-separator'
 
-export type SeparatorProps = React.ComponentPropsWithRef<typeof SeparatorPrimitive.Root> & {
-  extended?: boolean
-  position?: 'center' | 'start' | 'end'
-}
+export type SeparatorProps = React.ComponentPropsWithRef<typeof SeparatorPrimitive.Root>
 
 export function Separator({
   className,
   ref,
   orientation = 'horizontal',
   decorative = true,
-  position = 'start',
-  extended = false,
   ...props
 }: SeparatorProps) {
   return (
@@ -26,26 +21,7 @@ export function Separator({
       orientation={orientation}
       decorative={decorative}
       className={cn(
-        'relative flex items-center gap-1.5 text-xs whitespace-nowrap shrink-0',
-
-        // Horizontal mode needs to support inline text, use pseudo elements for lines
-        'data-[orientation=horizontal]:after:content-[""] data-[orientation=horizontal]:after:bg-fg-rev/20',
-        'data-[orientation=horizontal]:after:h-px data-[orientation=horizontal]:after:w-full',
-        'data-[orientation=horizontal]:before:content-[""] data-[orientation=horizontal]:before:bg-fg-rev/20',
-        'data-[orientation=horizontal]:before:h-px data-[orientation=horizontal]:before:w-full',
-
-        // Extended mode
-        // extended && 'before:border-t-fg-rev/20 before:w-2.5 before:border-t before:content-[""]',
-        'data-[extended=true]:data-[position=start]:data-[orientation=horizontal]:before:w-2.5',
-        'data-[extended=true]:data-[position=end]:data-[orientation=horizontal]:after:w-2.5',
-
-        // Positions
-        'data-[extended=false]:data-[position=end]:data-[orientation=horizontal]:after:hidden',
-        'data-[extended=false]:data-[position=start]:data-[orientation=horizontal]:before:hidden',
-
-        // Vertical mode does not need to support inline text, use background color instead
-        'data-[orientation=vertical]:bg-fg-rev/20',
-        'data-[orientation=vertical]:w-px data-[orientation=vertical]:h-full',
+        'bg-fg-rev/20 shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
         className
       )}
       {...props}
