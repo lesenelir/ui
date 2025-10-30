bd8b86fc17538f9f65e3326e479620a5fd1d8340
lesenelir <hi@lesenelir.me>
2025-10-30 13:52:50 +0800

    feat: implement variant-based styling for Spinner component with default variant

--- packages/ui/src/components/spinner/spinner.tsx
+++ packages/ui/src/components/spinner/spinner.tsx
@@ -3,23 +3,30 @@
 import type React from 'react'
 
 import { cn } from '@lesenelir/ui/lib/utils'
+import { cva, type VariantProps } from 'class-variance-authority'
 
-export type SpinnerProps = React.ComponentPropsWithRef<'span'> & {
-  variant?: 'circle' | 'radial' | 'quarter'
-}
+const spinnerVariants = cva('', {
+  variants: {
+    variant: {
+      radial: 'i-tabler-loader',
+      circle: 'i-tabler-loader-2',
+      quarter: 'i-tabler-loader-quarter',
+    },
+  },
+  defaultVariants: {
+    variant: 'circle',
+  },
+})
+
+export type SpinnerProps = React.ComponentPropsWithRef<'span'> &
+  VariantProps<typeof spinnerVariants>
 
 export function Spinner({ variant = 'circle', className, ref, ...props }: SpinnerProps) {
   return (
     <span
       ref={ref}
       data-slot={'spinner'}
-      className={cn(
-        variant === 'radial' && 'i-tabler-loader',
-        variant === 'circle' && 'i-tabler-loader-2',
-        variant === 'quarter' && 'i-tabler-loader-quarter',
-        'animate-spin',
-        className
-      )}
+      className={cn(spinnerVariants({ variant }), 'animate-spin', className)}
       {...props}
     />
   )
