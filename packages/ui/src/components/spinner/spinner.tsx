1d464ed0725c2bf805522bec3c647eb7ee7f0013
lesenelir <hi@lesenelir.me>
2025-10-30 13:40:10 +0800

    feat: add Spinner component with customizable variants and animation

---
+++ packages/ui/src/components/spinner/spinner.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import type React from 'react'
+
+import { cn } from '@lesenelir/ui/lib/utils'
+
+export type SpinnerProps = React.ComponentPropsWithRef<'span'> & {
+  variant: 'circle' | 'radial' | 'quarter'
+}
+
+export function Spinner({ variant, className, ref, ...props }: SpinnerProps) {
+  return (
+    <span
+      ref={ref}
+      data-slot={'spinner'}
+      className={cn(
+        variant === 'radial' && 'i-tabler-loader',
+        variant === 'circle' && 'i-tabler-loader-2',
+        variant === 'quarter' && 'i-tabler-loader-quarter',
+        'animate-spin',
+        className
+      )}
+      {...props}
+    />
+  )
+}
