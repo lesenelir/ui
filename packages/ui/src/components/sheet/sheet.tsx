f183f183232d59df70bd128d759c94ca12ea2e36
lesenelir <hi@lesenelir.me>
2025-11-03 13:45:57 +0800

    feat: enhance styling for Close button in Sheet component to improve accessibility and interaction

--- packages/ui/src/components/sheet/sheet.tsx
+++ packages/ui/src/components/sheet/sheet.tsx
@@ -75,6 +75,8 @@ export function SheetContent({ className, children, side = 'right', ...props }: 
           className={cn(
             'absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 data-[state=open]:bg-fg-rev/50',
             'focus-visible-ring',
+            "[&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+            "[&_span]:pointer-events-none [&_span]:shrink-0 [&_span:not([class*='size-'])]:size-4",
             'disabled:pointer-events-none'
           )}
         >
