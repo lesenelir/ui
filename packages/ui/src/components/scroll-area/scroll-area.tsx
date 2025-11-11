a1e4e258c344703af766f73694f481a9b09729d8
lesenelir <hi@lesenelir.me>
2025-11-11 16:04:05 +0800

    feat: implement ScrollArea and ScrollBar components for enhanced scrolling functionality

---
+++ packages/ui/src/components/scroll-area/scroll-area.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import type React from 'react'
+
+import { cn } from '@lesenelir/ui/lib/utils'
+import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area'
+
+export type ScrollBarProps = React.ComponentProps<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
+export type ScrollAreaProps = React.ComponentProps<typeof ScrollAreaPrimitive.Root>
+
+export function ScrollBar({ className, orientation = 'vertical', ...props }: ScrollBarProps) {
+  return (
+    <ScrollAreaPrimitive.ScrollAreaScrollbar
+      data-slot={'scroll-area-scrollbar'}
+      orientation={orientation}
+      className={cn(
+        'flex touch-none p-px transition-colors select-none',
+        orientation === 'vertical' && 'h-full w-2.5 border-l border-l-transparent',
+        orientation === 'horizontal' && 'h-2.5 flex-col border-t border-t-transparent',
+        className
+      )}
+      {...props}
+    >
+      <ScrollAreaPrimitive.ScrollAreaThumb
+        data-slot={'scroll-area-thumb'}
+        className={'bg-border relative flex-1 rounded-full'}
+      />
+    </ScrollAreaPrimitive.ScrollAreaScrollbar>
+  )
+}
+
+export function ScrollArea({ className, children, ...props }: ScrollAreaProps) {
+  return (
+    <ScrollAreaPrimitive.Root
+      data-slot={'scroll-area'}
+      className={cn('relative', className)}
+      {...props}
+    >
+      <ScrollAreaPrimitive.Viewport
+        data-slot={'scroll-area-viewport'}
+        className={cn(
+          'size-full rounded-[inherit] transition-[color,box-shadow] outline-none',
+          'focus-visible:ring-ac/30  focus-visible:ring-[3px] focus-visible:outline-1'
+        )}
+      >
+        {children}
+      </ScrollAreaPrimitive.Viewport>
+
+      <ScrollBar />
+      <ScrollAreaPrimitive.Corner />
+    </ScrollAreaPrimitive.Root>
+  )
+}
